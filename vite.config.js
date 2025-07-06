99b4106ec4e83d0cd1539a75a2ed094bb3e723e3
Mark Canup <markcanup@pobox.com>
2025-07-06 14:12:36 -0400

    Allow Netlify dev server hostname

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ['devserver-main--stately-vacherin-91d824.netlify.app']
+  }
 })
